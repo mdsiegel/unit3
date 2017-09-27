037d70527e9c94125e24837174f5a20f5e618be6
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-27 13:31:28 -0400

    Updated from Brython Server: 9/27/2017 1:31:27 PM

--- loopDemo.py
+++ loopDemo.py
@@ -6,5 +6,11 @@
 for i in range(1,6):
     print('I <3 Computer Science')
 """
+"""
 for i in range(1,21):
     print(i)
+"""
+#print out the odd numbers from 13 to 27
+for i in range(13,28):
+    if i%2==1:
+        print(i)
