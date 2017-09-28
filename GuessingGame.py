b189c8aef243d674075db0bf2e6ba54b9cbc2ac7
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-28 14:45:22 -0400

    Updated from Brython Server: 9/28/2017 2:45:21 PM

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,1 +1,13 @@
+#Matthew Siegel
+#9/28/17
+#GuessingGame.py- Guessing the numbers
 
+from random import randint
+
+num = randint(1,100)
+correct = False
+
+while correct == False:
+    guess = int(input('Guess a number between 1 and 100: '))
+    
+    
