bb118232c196c906aaf2a19a1d5e213d06439894
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-28 14:52:23 -0400

    Updated from Brython Server: 9/28/2017 2:52:22 PM

--- GuessingGame.py
+++ GuessingGame.py
@@ -5,14 +5,15 @@
 from random import randint
 
 num = randint(1,100)
-
+total = 0
 while 1<2:
+    total += 1
     guess = int(input('Guess a number between 1 and 100: '))
     if guess > num:
         print('Too high')
     elif guess < num:
         print('Too low')
     else:
-        print("correct")
+        print("correct in",total,"guesses")
         break
     
