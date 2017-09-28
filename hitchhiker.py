7fd591ad99af362d1d88438653c3298e72dafc10
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-28 14:34:42 -0400

    Updated from Brython Server: 9/28/2017 2:34:41 PM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,12 @@
+#Matthew Siegel
+#9/28/17
+#hitchhiker.py- learning how to do breaks
 
+quit = False
+
+while quite==False:
+    response = input("Ask a quesiton or type 'quit'")
+    if response == 'quit':
+        break
+    else:
+        print('42')
