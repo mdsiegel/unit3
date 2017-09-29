763a2395e314733e8877967d86681459ea289ae2
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-29 14:36:34 -0400

    Updated from Brython Server: 9/29/2017 2:36:33 PM

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,1 +1,18 @@
+#Matthew Siegel
+#9/29/17
+#perfectNumber.py - weird number game
 
+num = int(input('Enter a perfect number: '))
+total = 0
+i = 1
+while i<num:
+    if num%i == 0:
+        total +=i
+        print(i)
+    i+=1
+
+print(total)
+if total == num:
+    print('Perfect Number!')
+else:
+    print('Not perfect')
