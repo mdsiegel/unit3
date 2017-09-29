8fcf786745150282cf71e02ed10c7bb2191a6d8d
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-29 14:47:54 -0400

    Updated from Brython Server: 9/29/2017 2:47:52 PM

--- fri13.py
+++ fri13.py
@@ -1,1 +1,8 @@
+#Matthew Siegel
+#9/29/17
+#fri13 - finding the friday the 13ths
 
+from calendar import weekday
+from datetime import date
+
+print(date.today().month,date.today().day, date.today().year)
