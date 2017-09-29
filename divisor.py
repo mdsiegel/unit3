ffe197a20e0f5ce04918ce0b05ca5a4878d2b919
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-29 14:24:31 -0400

    Updated from Brython Server: 9/29/2017 2:24:29 PM

--- divisor.py
+++ divisor.py
@@ -1,1 +1,12 @@
+#Matthew Siegel
+#9/29/17
+#divisor.py - finding all the divisors
+
+num = int(input('Enter a number: '))
+stop = False
+i = 1
+while stop == False:
+    if num%i == 0:
+    
+    
 
