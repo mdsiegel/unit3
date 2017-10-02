7e1a67ada0737923956ce3634ff349512f3ed4b6
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-02 14:52:23 -0400

    Updated from Brython Server: 10/2/2017 2:52:22 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -2,4 +2,13 @@
 #9/29/17
 #changeComputerloop.py - finding amount of change
 
+cents = int(input('Enter an amount of cents: '))
+quarters = 0
 
+
+while cents > 0:
+    if cents // 25 > 1 :
+        quarters +=1
+        cents -=25
+    if cents //10 > 1:
+        
