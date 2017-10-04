c1fab481e80d151956fb7d46986fecc5f18bc0a5
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-04 13:50:45 -0400

    Updated from Brython Server: 10/4/2017 1:50:45 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -9,16 +9,16 @@ nickels = 0
 pennies = 0
 
 while cents > 0:
-    if cents // 25 > 1 :
+    if cents // 25 == 1 :
         quarters +=1
         cents -=25
-    elif cents //10 > 1:
+    elif cents //10 == 1:
         dimes += 1
         cents -=10
-    elif cents //5 > 1:
+    elif cents //5 == 1:
         nickels += 1
         cents -=5
-    elif cents //1 > 1:
+    elif cents //1 == 1:
         pennies += 1
         cents -=1
         
