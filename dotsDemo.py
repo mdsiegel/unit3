9697e02ab5ef8a4ab2cc54e7f85cc86eaf7b9d67
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-05 14:28:29 -0400

    Updated from Brython Server: 10/5/2017 2:28:30 PM

--- dotsDemo.py
+++ dotsDemo.py
@@ -8,6 +8,6 @@ red  = Color(0xFF0000,1)
 
 dot = CircleAsset(20,LineStyle(1,red),red)
 
-for i in range(10):
-    Sprite(dot,(20+50*i,20 + 50*i))
+for i in range(20):
+    Sprite(dot,(20+50*i,20))
 App().run()
