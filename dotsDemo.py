5782d8f2a73f32ef8ecad9a253806064fdd65c32
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-10-05 14:34:55 -0400

    Updated from Brython Server: 10/5/2017 2:34:55 PM

--- dotsDemo.py
+++ dotsDemo.py
@@ -6,9 +6,9 @@ from ggame import *
 
 red  = Color(0xFF0000,1)
 
-dot = CircleAsset(10,LineStyle(1,red),red)
+dot = CircleAsset(20,LineStyle(1,red),red)
 
-for j in range(42): #prints the row x times
-    for i in range(60): #prints one row of dots
-        Sprite(dot,(20*i,20*j))
+for j in range(12): #prints the row x times
+    for i in range(20): #prints one row of dots
+        Sprite(dot,(20+50*i,20+50*j))
 App().run()
